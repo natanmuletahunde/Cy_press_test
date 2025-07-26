4827b2a5b97d4b423a19a6b4dac639294284b210
pivnati <natanmuleta911@gmail.com>
2025-07-26 04:54:54 -0700

    create the custom command

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,14 +1,28 @@
-describe('Fundamentals test' , () =>{
-it('Contains correct header text ', ()=>{
-  cy.visit('/fundamentals')
-  cy.get('[data-test="fundamentals-header" ]').should('contain.text','Testing Fundamentals')
-})
-it('Accordion works correctly ', ()=>{
-  cy.visit('/fundamentals')
-  cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
-  cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
-  cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
-  cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
-  cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
-})
-})
+describe("Fundamentals test", () => {
+  beforeEach(() => {
+    cy.visit("/fundamentals");
+  });
+  it("Contains correct header text ", () => {
+    cy.get('[data-test="fundamentals-header" ]').should(
+      "contain.text",
+      "Testing Fundamentals"
+    );
+  });
+  it("Accordion works correctly", () => {
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
+});
