b0db8a95d03d16e47a1496e5ec51809ccab3e02c
pivnati <natanmuleta911@gmail.com>
2025-07-27 23:20:26 -0700

    finish the forms.cy.js

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,9 +4,20 @@ describe("form tests", () => {
   });
   it("Test subscribe form", () => {
     cy.contains(/testing forms/i);
-    cy.getDataTest('subscribe-form').find('input').type('natan@gmail.com')
-    cy.contains(/Successfully subbed:natan@gmail.com!/i).should('not.exist');
+    cy.getDataTest('subscribe-form').find('input').as('subscribe-input') // alias for the input field
+    cy.get('@subscribe-input').type('natan@gmail.com')  // calling the alias 
+    cy.contains(/Successfully subbed: natan@gmail.com!/i).should('not.exist');
     cy.getDataTest('subscribe-button').click();
-    cy.contains(/Successfully subbed:natan@gmail.com!/i).should('exist');
+    cy.contains(/Successfully subbed: natan@gmail.com!/i).should('exist');
+    cy.wait(3000);
+    cy.contains(/Successfully subbed: natan@gmail.com!/i).should('not.exist');
+    cy.get('@subscribe-input').type('natan@gmail.io');
+    cy.getDataTest('subscribe-button').click();
+    cy.contains(/Invalid email: natan@gmail.io!/i).should('exist')
+    cy.wait(3000);
+    cy.contains(/Invalid email: natan@gmail.io!/i).should('not.exist')
+    cy.contains(/fail!/i).should('not.exist');
+    cy.getDataTest('subscribe-button').click();
+    cy.contains(/fail!/i).should('exist');
   });
 });
