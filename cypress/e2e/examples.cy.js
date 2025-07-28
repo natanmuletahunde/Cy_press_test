c499a7f04882ddf9b408f0bc71bd7fc068c23363
pivnati <natanmuleta911@gmail.com>
2025-07-28 05:54:38 -1200

    use the cy.intercept for our request

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,9 +1,19 @@
 describe('Various example', () => {
     beforeEach(()=>{
-        cy.visit('/examples')
+        cy.visit('/')
     })
  it('multi-page testing' ,()=>{
     cy.getDataTest('nav-why-cypress').click()
     cy.location('pathname').should('equal','/');
+
+    cy.getDataTest('nav-overview').click()
+    cy.location('pathname').should('equal','/overview');
+
+    cy.getDataTest('nav-fundamentals').click()
+    cy.location('pathname').should('equal','/fundamentals')
+    
+    cy.getDataTest('nav-examples').click();
+    cy.location('pathname').should('equal','/examples');
+
  })
 })
