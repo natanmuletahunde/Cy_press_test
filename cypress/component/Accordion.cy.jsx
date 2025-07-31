32e4e709cc28a44c5f784fee9355c084d06afe8c
pivnati <natanmuleta911@gmail.com>
2025-07-31 04:20:23 -1200

    test the first itmes in the accordion list items

--- cypress/component/Accordion.cy.jsx
+++ cypress/component/Accordion.cy.jsx
@@ -30,5 +30,13 @@ const items = [
 describe('Accordion.cy.jsx', () => {
   it('Items Accordion ', () => {
     cy.mount(<ItemsAccordion items={items}/>)
+    cy.getDataTest('accordion-wrapper').within(()=>{
+      cy.get('[data-test^="accordion-item"]').should('have.length',3)
+    })
+  cy.contains('Your tests will exist in a describe block').should('not.be.visible')
+  cy.getDataTest('accordion-item-1').within(()=>cy.get('[role=button]').click())
+  cy.contains('Your tests will exist in a describe block').should('be.visible')
+  cy.getDataTest('accordion-item-1').within(()=>cy.get('[role=button]').click())
+  cy.contains('Your tests will exist in a describe block').should('not.be.visible')
   })
 })
